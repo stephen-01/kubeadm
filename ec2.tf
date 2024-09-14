2a9ac7625a4be90c47a59629ad79e6a382eee56d
adekunle <ceeepath@gmail.com>
2024-09-13 23:45:44 -0400

    kubeadm

---
+++ ec2.tf
@@ -0,0 +1,181 @@
+# configured aws provider with proper credentials
+provider "aws" {
+  region  = "us-east-1"
+}
+
+
+# create default vpc if one does not exit
+resource "aws_default_vpc" "default_vpc" {
+
+  tags = {
+    Name = "default vpc"
+  }
+}
+
+
+# use data source to get all avalablility zones in region
+data "aws_availability_zones" "available_zones" {}
+
+
+# create default subnet if one does not exit
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = data.aws_availability_zones.available_zones.names[0]
+
+  tags = {
+    Name = "default subnet"
+  }
+}
+
+
+# create security group for the ec2 instance
+resource "aws_security_group" "ec2_security_group6" {
+  name        = "ec2 security group6"
+  description = "allow access on required ports"
+  vpc_id      = resource.aws_default_vpc.default_vpc.id
+
+  # ingress {
+  #   description = "http access"
+  #   from_port   = 80
+  #   to_port     = 80
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
+
+  ingress {
+    description = "k8s access"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # ingress {
+  #   description = "k8s1 access"
+  #   from_port   = 10250
+  #   to_port     = 10250
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
+
+  # ingress {
+  #   description = "k8s2 access"
+  #   from_port   = 6443
+  #   to_port     = 6443
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
+
+  # ingress {
+  #   description = "k8s3 access"
+  #   from_port   = 31111
+  #   to_port     = 31111
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
+
+  # ingress {
+  #   description = "https access"
+  #   from_port   = 443
+  #   to_port     = 443
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
+
+  # ingress {
+  #   description = "ssh access"
+  #   from_port   = 22
+  #   to_port     = 22
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "k8s server sg"
+  }
+}
+
+
+# use data source to get a registered amazon linux 2 ami
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+# launch the k8s ec2 instance
+resource "aws_instance" "ec2_instance" {
+  ami                    = data.aws_ami.amazon_linux_2.id
+  instance_type          = "t2.medium"
+  subnet_id              = aws_default_subnet.default_az1.id
+  vpc_security_group_ids = [aws_security_group.ec2_security_group6.id]
+  key_name               = "project"
+  count                  = 2
+
+  tags = {
+    Name = "kubernetes server"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("~/Desktop/project.pem")
+    host = self.public_ip
+  }
+
+  provisioner "file" {
+    source      = "install_k8s.sh"
+    destination = "/tmp/install_k8s.sh"
+
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+        "sudo chmod +x /tmp/install_k8s.sh",
+        "sudo su -c'bash /tmp/install_k8s.sh'",
+    ]
+  }
+}
+
+# launch the kubectl instance
+resource "aws_instance" "kubectl" {
+  ami                    = data.aws_ami.amazon_linux_2.id
+  instance_type          = "t2.micro"
+  subnet_id              = aws_default_subnet.default_az1.id
+  vpc_security_group_ids = [aws_security_group.ec2_security_group6.id]
+  key_name               = "project"
+  user_data = "${file("install_kubectl.sh")}"
+
+  tags = {
+    Name = "kubectl server"
+  }
+}
+
+# print the url of the container
+output "container_url" {
+ value = ["${aws_instance.ec2_instance.*.public_ip}"]
+}
+
+# print the ssh command to login to the servers
+output "ssh_k8s_command" {
+  value = [for ip in aws_instance.ec2_instance[*].public_ip : "ssh -i project.pem ec2-user@${ip}"]
+}
+
+output "ssh_kubectl_command" {
+  value     = join ("", ["ssh -i project.pem ec2-user@", aws_instance.kubectl.public_ip])
+}
---
+++ install_kubectl.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Update the system
+sudo yum update -y
+
+# Install kubectl
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/repodata/repomd.xml.key
+exclude=kubelet kubeadm kubectl cri-tools kubernetes-cni
+EOF
+
+sudo yum install -y kubectl --disableexcludes=kubernetes
